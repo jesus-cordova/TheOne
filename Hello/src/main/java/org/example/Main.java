66be562b93b6bfe7615f4c9a7217effce9387ed6
Jesus Cordova <jjcorodvajj@outlook.com>
2023-02-01 20:24:08 -0500

    the one

---
+++ Hello/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person newPerson = new Person("Jon Jones");
+        System.out.println(newPerson.getFirstName());
+    }
+}
---
+++ Hello/src/main/java/org/example/Person.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Person {
+    String firstName;
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+     
+}
